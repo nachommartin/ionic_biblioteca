941c44e4c0586993ec0b0cbaf2017109e94387ca
Estudiante <ignmmartin@gmail.com>
2022-02-09 13:24:41 +0000

    implementando el escaneo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
-
+import { BarcodeScanner } from '@ionic-native/barcode-scanner/ngx';
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
@@ -12,7 +12,8 @@ import { HttpClientModule } from '@angular/common/http';
   declarations: [AppComponent],
   entryComponents: [],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, HttpClientModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy},
+  BarcodeScanner],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/app/biblioteca/biblioteca.page.ts
+++ src/app/biblioteca/biblioteca.page.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { Libro } from '../interfaces/libro';
 import { BiblioServService } from '../services/biblio-serv.service';
+import { BarcodeScanner, BarcodeScannerOptions } from '@ionic-native/barcode-scanner/ngx';
 
 @Component({
   selector: 'app-biblioteca',
@@ -10,12 +11,28 @@ import { BiblioServService } from '../services/biblio-serv.service';
 })
 export class BibliotecaPage implements OnInit {
 
+
+  encodedData: any;
+  scannedBarCode: {};
+  barcodeScannerOptions: BarcodeScannerOptions;
+
+
   libros:Array<Libro>
   busqueda:string; 
   isbn:string;
 
 
-  constructor(private servicio:BiblioServService, private router: Router) { }
+  constructor(private servicio:BiblioServService, private router: Router, private scanner: BarcodeScanner) { 
+
+    this.encodedData = "Programming isn't about what you know";
+      
+    this.barcodeScannerOptions = {
+      showTorchButton: true,
+      showFlipCameraButton: true
+    };
+
+
+  }
 
   ngOnInit() {
   }
@@ -38,6 +55,15 @@ export class BibliotecaPage implements OnInit {
   this.isbn=pk_isbn;
   console.log("isbn")
   this.router.navigate(["/libro", pk_isbn]);
+}
+
+escanear(){
+  this.scanner.scan().then(barcodeData => {
+      console.log('Barcode data', barcodeData);
+      this.scannedBarCode=barcodeData;
+  }).catch(err => {
+    console.log('Error', err);
+});
 }
 
 }
